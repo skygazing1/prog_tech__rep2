3f23aa8ab248fd42b63730330c844bfb205f476e
skygazing1 <8120007@gmail.com>
2024-12-05 21:19:45 +0300

    обновление id для файла 1

--- main.py
+++ main.py
@@ -11,3 +11,8 @@ df1 = pd.read_excel(file1_path)
 df2 = pd.read_excel(file2_path)
 df3 = pd.read_excel(file3_path)
 
+# обновление идентификаторов
+
+# для файла 1 с 1
+df1['id'] = range(1, len(df1) + 1)
+
