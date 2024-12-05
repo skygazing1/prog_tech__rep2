883353f76be4ee677b7014b1c5e19c4a7d208b70
skygazing1 <8120007@gmail.com>
2024-12-05 21:13:19 +0300

    Считывание данных из файлов

---
+++ main.py
@@ -0,0 +1,13 @@
+
+import pandas as pd
+
+# считывание данных из файлов
+
+file1_path = '1.xlsx'
+file2_path = '2.xlsx'
+file3_path = '3.xlsx'
+
+df1 = pd.read_excel(file1_path)
+df2 = pd.read_excel(file2_path)
+df3 = pd.read_excel(file3_path)
+
